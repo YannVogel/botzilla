c6eaa24ce2fff1bae346c6e6d609448524629ca8
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-09 11:59:18 +0100

    Added test command

---
+++ commands/test.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'test',
+    description: '',
+    args: false,
+    usage: '',
+    guildOnly: true,
+    cooldown: 60,
+    execute(message, args) {
+
+        console.log(`Commande de test réclamée par ${message.author.username}!`)
+        message.channel.send(`Commande de test réclamée par ${message.author}!`);
+    }
+};
