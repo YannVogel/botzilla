644ced5909e3aa061ceb4aa7bcfe46db10ce030f
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-09 01:57:55 +0100

    egs created
    command egs created

--- commands/egs.js
+++ commands/egs.js
@@ -1,38 +1,33 @@
 const {prefix} = require('../config');
 const rp = require('request-promise');
-const $ = require('cheerio');
+const cheerio = require('cheerio');
+
 
 module.exports = {
     name: 'egs',
     description: 'Affiche le(s) jeu(x) gratuit(s) de la semaine sur l\'Epic Game Store',
-    args: true,
-    usage: '<langage> <notion>',
+    args: false,
+    usage: '',
     guildOnly: false,
     cooldown: 0,
     execute(message, args) {
-        if (!args[1]) return message.channel.send(`\nLa bonne syntaxe est : \`${prefix}${this.name} ${this.usage}\``);
-        if (args[0].toLowerCase() === 'php' && args[1]) {
-            //Tout ce qui concerne la documentation PHP
+        const url = 'https://www.reddit.com/r/GameDeals/search?q=site:epicgames.com+OR+title:epicgamestore+OR+title:%22epic+game+store%22+OR+title:%22EGS%22+OR+title:%22epic+games%22&restrict_sr=on&sort=new&include_over_18=on&feature=legacy_search';
 
-            const url = `https://www.php.net/${args[1]}`;
+        rp(url)
+            .then(html => {
+                //success!
+                const $ = cheerio.load(html);
+                const newsTab = $('h3._eYtD2XCVieq6emjKBH3m', html).text().split(/\[Epic Games Store\] /);
+                const lastNew = newsTab.shift();
 
-            rp(url)
-                .then(html => {
-                    //success!
-                    let variable = $.load(html);
+                return message.reply("```" +
+                    newsTab[0]
+                    + "```\n <https://www.epicgames.com/store/fr/free-games>");
 
-                    message.reply("```" +
-                        $('.methodsynopsis', html).text()+
-                        "```");
-
-                })
-                .catch(err =>{
-                    //handle error
-                    console.log(err);
-                });
-
-            return message.reply(`<https://www.php.net/${args[1]}>`);
-        }
-
+            })
+            .catch(err =>{
+                //handle error
+                console.log(err);
+            });
     }
 };
