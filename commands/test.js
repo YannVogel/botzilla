86ffd33c315ca8d451a81dc2c14759dce253d4a8
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-19 22:29:33 +0100

    Update twitch.js
    Security @ commands/twitch.js

--- commands/test.js
+++ commands/test.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 module.exports = {
     name: 'test',
     description: '',
@@ -8,6 +10,13 @@ module.exports = {
     cooldown: 60,
     execute(message, args) {
 
-        return message.channel.send(`Commande de test réclamée par ${message.author}!`);
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+            .then(response => {
+                message.channel.send(response.data.url);
+                message.channel.send(response.data.explanation);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 };
