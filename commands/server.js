7ad18835fbae24cb85e341da0095c0f5516d27d0
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-31 12:36:47 +0100

    Update server command
    :repeat: Update : commands/server.js.

--- commands/server.js
+++ commands/server.js
@@ -1,12 +1,63 @@
+const Discord = require('discord.js');
+const {botAvatar} = require('../config');
+
+const daysFr = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+];
+
+const monthsFr = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"
+];
+
+function embedMessage(message, creationDate){
+    return new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle(message.guild.name)
+        .setAuthor(`Propriétaire : ${message.guild.owner.user.username}`, message.guild.owner.user.avatarURL)
+        .setDescription(`Serveur créé le ${creationDate}`)
+        .setThumbnail(botAvatar)
+        .addField("Nombre de membres", message.guild.memberCount, true)
+        .addField("Nombre d'emojis", message.guild.emojis.size, true)
+        .setImage(message.guild.iconURL || message.guild.nameAcronym)
+}
+
 module.exports = {
     name: 'server',
     description: 'Affiche le nom du serveur et le nombre de membres',
     args: false,
-    usage: '',
+    usage: 'bz!server',
     guildOnly: true,
     adminOnly: false,
     cooldown: 60,
     execute(message, args) {
-        message.channel.send(`Nom du serveur : ${message.guild.name}\nNombre de membres : ${message.guild.memberCount}`);
+        const dateRaw = message.guild.createdAt;
+
+        const dayFr = daysFr[dateRaw.getDay()];
+        const dayNumber = dateRaw.getDate();
+        const monthFr = monthsFr[dateRaw.getMonth()];
+        const year = dateRaw.getFullYear();
+
+        const dateFr = `${dayFr} ${dayNumber} ${monthFr} ${year}`;
+
+        const finalMessage = embedMessage(message, dateFr);
+
+        message.channel.send(finalMessage);
     }
 };
