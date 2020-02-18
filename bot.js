9cc0103341cea322170c35437f7172b379b6ca63
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-02-18 20:37:57 +0100

    Refactoring egsFetcher & steamFetcher
    Refactoring commands in order to disply messages in an apropriate order.
    
    ✍️ Edit : bot.js ;
    ✍️ Edit : egsFetcher.js ;
    ✍️ Edit : steamFetcher.js.

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const bot = new Discord.Client();
 const welcomeMessageAndStreamNotificationChannel = 'discussion';
 const salesChannel = 'botzilla';
 const adminChannelName = 'botzilla-admin';
-const roleToMention = 'Infos';
+const roleToMentionForGamesSales = 'Infos';
+const roleToMentionForWolcenNews = 'wolcen';
 
 // Bot connexion
 const login = require('./bot/login');
@@ -25,9 +26,9 @@ onMessage(bot);
 // Displays a @here message when Bobz starts streaming
 const streamStarting = require('./bot/automated/streamStarting');
 streamStarting(bot, 60, welcomeMessageAndStreamNotificationChannel);
-// Displays a @everyone message when new EGS deals are available
+// Displays a @roleToMentionForGamesSales message when new EGS deals are available
 const egsFetcher = require('./bot/automated/egsFetcher');
-egsFetcher(bot, 10, salesChannel, roleToMention);
-// Displays @everyone message when new Steam deals are available
+egsFetcher(bot, 10, salesChannel, roleToMentionForGamesSales);
+// Displays @roleToMentionForGamesSales message when new Steam deals are available
 const steamFetcher = require('./bot/automated/steamFetcher');
-steamFetcher(bot, 10, salesChannel, adminChannelName, roleToMention);
+steamFetcher(bot, 10, salesChannel, adminChannelName, roleToMentionForGamesSales);
--- bot/automated/egsFetcher.js
+++ bot/automated/egsFetcher.js
@@ -51,12 +51,16 @@ module.exports = (botClient, timeInMinutes, channelName, roleToMention) => {
                             const role = guild.roles.find(role => role.name === roleToMention);
                             if(!role) {
                                 console.error(`egsFetcher : Je n'ai pas trouvé le rôle ${roleToMention} sur le serveur ${guild.name}...`);
-                                channel.send(`J'ai trouvé une nouvelle promo intéressante sur l'Epic Games Store !`);
-                                return channel.send(response);
+                                channel.send(`J'ai trouvé une nouvelle promo intéressante sur l'Epic Games Store !`)
+                                    .then(() => {
+                                        return channel.send(response)
+                                    });
                             }
 
-                            channel.send(`<@&${role.id}> J'ai trouvé une nouvelle promo intéressante sur l'Epic Games Store !`);
-                            return channel.send(response);
+                            channel.send(`<@&${role.id}> J'ai trouvé une nouvelle promo intéressante sur l'Epic Games Store !`)
+                                .then(() => {
+                                    return channel.send(response);
+                                });
                         });
                     });
             });
