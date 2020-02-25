4f1379cb23f7486559dbfcfd7f60c33b90de376a
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-02-25 20:38:14 +0100

    Creating sheet management for (new) players
    Manage players and new players sheet in DB.
    
    🟢 Creating : commands/dependencies/_getPlayerSheet.js ;
    🟢 Creating : commands/fiche.js ;
    🟢 Creating : models/playerSheet.js.

---
+++ commands/dependencies/_getPlayerSheet.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const PlayerSheet = require('../../models/playerSheet');
+
+const daysFr = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+];
+
+const monthsFr = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"
+];
+
+module.exports = {
+    getPlayerSheet: function(member) {
+        return new Promise(resolve =>{
+            PlayerSheet.findOne({playerId: member.id})
+                .then(player => {
+                    // If this player doesn't have a sheet in the DB...
+                    if (!player) {
+                         player = new PlayerSheet({
+                            playerId: member.id,
+                            playerName: member.username,
+                            playerExperience: 0,
+                            playerPurse: 0,
+                            playerInventory: [],
+                            createdAt: new Date()
+                        });
+                        // ...creates the player sheet in the DB
+                        player.save().catch(console.error);
+                    }
+
+                    const dayFr = daysFr[player.createdAt.getDay()];
+                    const dayNumber = player.createdAt.getDate();
+                    const monthFr = monthsFr[player.createdAt.getMonth()];
+                    const year = player.createdAt.getFullYear();
+
+                    const dateFr = `${dayFr} ${dayNumber} ${monthFr} ${year}`;
+
+                    resolve(
+                        new Discord.MessageEmbed()
+                            .setColor('#ffffff')
+                            .setTitle(player.playerName)
+                            .setDescription(`Fiche créée le ${dateFr}`)
+                            .addField("Expérience", player.playerExperience, true)
+                            .addField("Inventaire", `${player.playerInventory.length} objet${player.playerInventory.length > 1 ? 's' : ''}`, true)
+                            .addField("Bourse", player.playerPurse, true)
+                            .setThumbnail(member.avatarURL())
+                    )
+                });
+        })
+    }
+};
---
+++ models/playerSheet.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const playerSheetSchema = mongoose.Schema({
+    playerId: {type: String, required: true, unique: true},
+    playerName: {type: String, required: true},
+    playerExperience: {type: Number, required: true},
+    playerPurse: {type: Number, required: true},
+    playerInventory: {type: Array, required: true},
+    createdAt: {type: Date, required: true}
+});
+
+playerSheetSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('playerSheet', playerSheetSchema);
