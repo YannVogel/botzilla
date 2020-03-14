cedbe90540e265ecdd236a435b21c5f3b967def8
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-03-14 18:18:45 +0100

    Creating !materials command
    🟢 Creating : materials.js.

---
+++ commands/materials.js
@@ -0,0 +1,22 @@
+const PlayerSheet = require('../models/playerSheet');
+const {prefix} = require('../config');
+const format = require('./dependencies/_getFormattedPlayerInventory');
+const cd = require('./dependencies/_deleteTimer');
+
+module.exports = {
+    name: 'materials',
+    description: `Renseigne un joueur sur ses matériaux.`,
+    cooldown: 60,
+    execute(message) {
+        PlayerSheet.findOne({playerId: message.author.id})
+            .then(player => {
+                if(!player)
+                {
+                    cd.deleteTimer(message.author.id, this.name);
+                    return message.reply(`Merci de commencer par créer ta fiche avec la commande ${prefix}fiche !`)
+                }
+
+                return message.channel.send(`🌠 Matériaux possédés par <@${message.author.id}> : \n${format.getFormattedPlayerInventory(player.playerMaterials, true, false)}`);
+            })
+    }
+};
