d7a20913c7c89a5f79f54d1e95f918a90a84d483
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-05-04 20:05:03 +0200

    Adding leaveAllGuild function

---
+++ bot/leaveAllGuild.js
@@ -0,0 +1,17 @@
+module.exports = (botClient, check, timeInSeconds) => {
+    botClient.setInterval(() => {
+        if(check) {
+            botClient.guilds.cache.forEach(guild => {
+                if(guild.id === '337706201452249088') {
+                    guild.owner.send(`J'ai quitté le serveur **${guild.name}** suite à une décision de l'administrateur...`)
+                        .then(() => {
+                            guild.leave()
+                                    .then(g => console.log(`Left the guild ${g}`))
+                                    .catch(console.error)
+                            }
+                        );
+                }
+            });
+        }
+    },1000 * timeInSeconds);
+};
